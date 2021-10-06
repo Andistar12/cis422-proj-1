dfb190ce59b5d5a9689cfae96709c47dda5a4605
Pranav Mathur <pranavmathur99@gmail.com>
2021-10-05 21:30:11 -0700

    Add command line option for config location

--- backend.py
+++ backend.py
@@ -1,6 +1,7 @@
 # serve pages using Flask
 
 import os # Used to fetch environment vars for parameters
+import sys # Used to access command-line parameters
 import json # Used for config loading
 import logging # Used for logging
 import flask # Used as the backend and for webpage rendering
@@ -10,12 +11,19 @@ import tsp # Our TSP solver
 
 app = flask.Flask(__name__)
 
+# Determine the appropriate config file location
+cfgloc = ""
+try:
+    cfgloc = os.environ["CONFIG_LOC"]
+except KeyError:
+    # config file location can be specified via command line argument
+    if len(sys.argv) > 1 and os.path.exists(sys.argv[1]):
+        cfgloc = sys.argv[1]
 
 # Read files from config 
 try:
-    cfgfile = open(os.environ["CONFIG_LOC"])
-    config = json.load(cfgfile)
-    cfgfile.close()
+    with open(cfgloc) as cfgfile:
+        config = json.load(cfgfile)
 except Exception:
     logging.getLogger("backend").error("Error occurred opening config file", exc_info=True)
     config = {}
