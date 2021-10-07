6cea76cbaa87204badb01045ca9ad70e72ca7db8
Pranav Mathur <pranavmathur99@gmail.com>
2021-10-06 17:24:02 -0700

    Change initial coordinates from Anstett to Deschutes

--- static/script.js
+++ static/script.js
@@ -7,8 +7,8 @@ var markers = [];
 var map;
 function initMap() {
     map = new google.maps.Map(document.getElementById("google-map"), {
-        center: {lat:  44.0457, lng: -123.0772}, //latitude/longitude of Anstett Hall
-        zoom: 18,
+        center: {lat:  44.0458, lng: -123.0711}, //latitude/longitude of Deschutes Hall
+        zoom: 19,
         streetViewControl: false
     });
     map.addListener("click", function(event) {
