2c1f2e49d4ba24f5024d08cd5fca2c5f16e244f2
Nate Koch <nkoch@jaaku.xyz>
2021-10-12 21:17:51 -0700

    implemented most of tsp tests still need to add randomly generated mtx

--- tsp_tests.py
+++ tsp_tests.py
@@ -1,9 +1,34 @@
 import unittest
-import tsp
+from tsp import tsp
 
 class TestTSP(unittest.TestCase):
-    def test_1(self):
-        pass
+    def test_empty_mtx(self):
+        self.assertEqual(tsp([[]]), [0])
+
+    def test_mtx_1x1(self):
+        self.assertEqual(tsp([[0]]), [0])
+
+    def test_mtx_2x2(self):
+        self.assertEqual(tsp([[0,1],
+                              [1,0]]), [0,1])
+
+    def test_mtx_3x3(self):
+        self.assertEqual(tsp([[0,2,4],
+                              [2,0,3],
+                              [4,3,0]]), [0,1,2])
+
+    def test_mtx_arbitrary(self):
+        # TODO: Change mtx to be random
+        # TODO: Add check if all nodes in the mtx
+        mtx = [[0, 9, 75, 0, 0],
+                [9, 0, 95, 19, 42],
+                [75, 95, 0, 51, 66],
+                [0, 19, 51, 0, 31],
+                [0, 42, 66, 31, 0]] # change to be random
+        tsp_solution = tsp(mtx)
+        self.assertEqual(len(mtx[0]), len(tsp_solution))
+        self.assertEqual(tsp_solution[0], 0)
+        # check if all nodes in mtx
 
 if __name__ == '__main__':
     unittest.main()
