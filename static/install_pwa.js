09a2db3afdeffea577390ed0c563a2c93ae47e29
Andy Nguyen <nguyenandy41@gmail.com>
2021-10-17 18:08:22 -0700

    Add PWA install button and fix logo locations

---
+++ static/install_pwa.js
@@ -0,0 +1,25 @@
+// Manages the prompt to install the app PWA
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+});
+
+function installPWA() {
+    if ("deferredPrompt" in window) {
+        // Show the install prompt
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the install prompt');
+        } else {
+            console.log('User dismissed the install prompt');
+        }
+        });
+    }
+}
