25ba44a3134d64c39dc0ccd15906b9905cf2b9ed
theonering3 <theonering296491895@hotmail.com>
2021-11-02 19:00:09 -0700

    commenting the code

--- tsp.py
+++ tsp.py
@@ -1,6 +1,21 @@
 # find an optimal path between multiple points
 
-""" (Ants Colony Optimizer)"""
+"""
+(Ants Colony Optimizer)
+
+    Calculates the Travelling-Salesman solution for the given graph
+
+    Nodes are identified from 0 to n - 1, where n = len(mtx)
+
+    The graph is given in adjacency matrix form, where the first index is
+    source and second index is destination. For example, mtx[1][2] is the
+    weighted cost of travelling from node 1 to node 2.
+
+    Parameters:
+     - mtx: The square adjacency matrix
+    Returns:
+     - A length n list of the calculated permutation, where 0 is always the first entry
+"""
 
 import random
 from random import randrange
@@ -221,20 +236,6 @@ def tsp(mtx):
 
  # (Prim's Method)
 def tsp2(mtx):
-    """
-    Calculates the Travelling-Salesman solution for the given graph
-
-    Nodes are identified from 0 to n - 1, where n = len(mtx)
-
-    The graph is given in adjacency matrix form, where the first index is 
-    source and second index is destination. For example, mtx[1][2] is the 
-    weighted cost of travelling from node 1 to node 2.
-
-    Parameters:
-     - mtx: The square adjacency matrix
-    Returns:
-     - A length n list of the calculated permutation, where 0 is always the first entry
-    """
 
 
     n = len(mtx)
